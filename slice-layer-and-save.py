0aa8be73c942f61cf88ec81c68b034e51461608e
irian-codes <irianplus1@hotmail.com>
2024-04-18 11:19:28 +0200

    Bump version to v1.1.0

--- slice-layer-and-save.py
+++ slice-layer-and-save.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# v1.0.1
+# v1.1.0
 
 import os, glob, json
 from gimpfu import *
@@ -273,3 +273,4 @@ main()
 # CHANGELOG:
 # v1.0.0: Initial version.
 # v1.0.1: Fixed overwriting existing files.
+# v1.1.0: Skipping odd or even layers if specified.
