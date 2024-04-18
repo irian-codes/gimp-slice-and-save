d41d2bfc43e773fba51ce32839051a6771934907
irian-codes <irianplus1@hotmail.com>
2024-04-18 07:15:04 +0200

    Inserting bottom guide too to create all rectangles

--- slice-pdf-and-save.py
+++ slice-pdf-and-save.py
@@ -117,7 +117,7 @@ def slice_layer_and_save(image, drawable, skipPages):
     guidesH.sort(key=lambda guide: guide.position)
     guidesV.sort(key=lambda guide: guide.position)
 
-    tryInsertRightImageGuide(image, drawable, guidesV)
+    tryInsertHelperGuides(image, drawable, guidesV, guidesH)
 
     pdb.gimp_message("heeey 1.2, H guides: " + ", ".join([guide.to_json() for guide in guidesH]))
     pdb.gimp_message("heeey 1.3, V guides: " + ", ".join([guide.to_json() for guide in guidesV]))
@@ -151,21 +151,25 @@ def exit_script(image, message):
     pdb.gimp_undo_push_group_end(image)
     pdb.gimp_message(message)
 
-def tryInsertRightImageGuide(image, drawable, guidesV):
-   """	
-   Tries to insert a guide to the right side of the image.
-   This eases the process of creating rectangles from guides.
-   """
+def tryInsertHelperGuide(image, drawable, guides, orientation):
    guideFound = False
    imageWidth = pdb.gimp_image_width(image)
 
-   for guide in reversed(guidesV):
+   for guide in reversed(guides):
        if guide.position == imageWidth:
         guideFound = True
         break
     
    if guideFound == False:
-        pdb.script_fu_guide_new_percent(image, drawable, 1, 100)
+        pdb.script_fu_guide_new_percent(image, drawable, orientation, 100)
+
+def tryInsertHelperGuides(image, drawable, guidesV, guidesH):
+   """	
+   Tries to insert a guide to the right side of the image.
+   This eases the process of creating rectangles from guides.
+   """
+   tryInsertHelperGuide(image, drawable, guidesH, 0)
+   tryInsertHelperGuide(image, drawable, guidesV, 1)
 
 
 register(
